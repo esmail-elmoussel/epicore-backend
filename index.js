daf637fb66d46dacb1c7e9f8459d0a35513df620
Esmail <elmoussel@gmail.com>
2020-11-27 03:50:37 +0200

    connect to DATABASE

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
+
+// connect to DATABASE
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+const db = mongoose.connection;
+db.on("error", () => console.log("connection error"));
+db.once("open", () => console.log("connected successfuly"));
 
 // end-points
 app.get("/", (req, res) => {
