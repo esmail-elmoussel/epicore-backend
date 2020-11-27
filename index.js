f8936e47955394c5e066265d6ced9b70027526b2
Esmail <elmoussel@gmail.com>
2020-11-27 15:09:43 +0200

    allow CORS from everywhere to access from website

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 // connect to DATABASE
 mongoose.connect(process.env.DATABASE_URL, {
