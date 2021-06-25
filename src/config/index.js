16f24dbc67a74f2cc45247d68ed2eed7b64538be
Esmail Elmoussel <elmoussel12@gmail.com>
2021-06-25 05:58:59 +0200

    fix deployment error

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 const envFound = dotenv.config();
-if (envFound.error) {
+if (envFound.error && process.env.NODE_ENV === "development") {
   throw new Error("Couldn't find .env file");
 }
 
