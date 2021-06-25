72f54948de86dee85036f8f56d1a042ea71cf7e5
Esmail Elmoussel <elmoussel12@gmail.com>
2021-06-25 06:58:45 +0200

    remove auth for testing

--- src/app.js
+++ src/app.js
@@ -15,13 +15,13 @@ const startServer = async () => {
     dataSources: () => ({
       discounts: new DiscountsDatasource(DiscountModal),
     }),
-    context: ({ req }) => {
-      const token = req.headers.authorization;
-      const authorized = token === config.AUTH_PASSWORD;
+    // context: ({ req }) => {
+    //   const token = req.headers.authorization;
+    //   const authorized = token === config.AUTH_PASSWORD;
 
-      // we can validate users here and get there data and role, but since we dont have any users, it's just dummy text password for our server!
-      if (!authorized) throw new Error("It is a private server!");
-    },
+    //   // we can validate users here and get there data and role, but since we dont have any users, it's just dummy text password for our server!
+    //   if (!authorized) throw new Error("wehooo!!! It is a private server.");
+    // },
   });
 
   server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
